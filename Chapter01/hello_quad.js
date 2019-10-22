1b3a8c7cdcddd21dffde59946eb93a95797516f2
blackcatcjy <cuijunyu@gmail.com>
2019-10-22 10:53:02 +0800

    Hello Quad

---
+++ Chapter01/hello_quad.js
@@ -0,0 +1,94 @@
+// HelloTriangle.js (c) 2012 matsuda
+// Vertex shader program
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' +
+  '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE =
+  'void main() {\n' +
+  '  gl_FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n' +
+  '}\n';
+
+function main() {
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('webgl');
+  
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // Write the positions of vertices to a vertex shader
+  var n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the positions of the vertices');
+    return;
+  }
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0, 0, 0, 1);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+
+}
+
+function initVertexBuffers(gl) {
+
+  var vertices = new Float32Array([
+      0.5,  0.5, 0.0,  // top right
+      0.5, -0.5, 0.0,  // bottom right
+      -0.5, -0.5, 0.0,  // bottom let
+      -0.5,  0.5, 0.0   // top let 
+  ]);
+
+    var indices = new Uint8Array([       // Indices of the vertices
+    0, 1, 3,  // first Triangle
+    1, 2, 3   // second Triangle
+    ]);
+
+  var n = 6; // The number of vertices
+
+  // Create a buffer object
+  var vertexBuffer = gl.createBuffer();
+  var indicesBuffer = gl.createBuffer();
+  if (!vertexBuffer || !indicesBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write date into the buffer object
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+  // Write the indices to the buffer object
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  return n;
+}
